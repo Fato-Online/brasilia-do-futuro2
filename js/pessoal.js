618eb13f45c23518aac0fd7b0fc98b86952b41c5
alexandrefnsc <alexandre.fnsc@gmail.com>
2016-05-06 18:31:52 -0300

    atualização do eixo tematico e inclusao de codigo que apaga o cache

--- js/pessoal.js
+++ js/pessoal.js
@@ -223,4 +223,38 @@ $(document).ready(function(){
 			});
 		},1000);
 	});
+
+	// GALERIA
+		Galleria.loadTheme('https://rawgit.com/fatoonline/carnaval-2016/master/themes/classic/galleria.classic.min.js');
+		Galleria.run('.galleria');
+
+		//FOTOS
+		var fotos = [
+         		"photo/01.jpg",
+         		"photo/02.jpg",
+         		"photo/03.jpg",
+         		"photo/04.jpg",
+         		"photo/05.jpg",
+         		"photo/06.jpg",
+         		"photo/07.jpg",
+         		"photo/08.jpg",
+         		"photo/09.jpg",
+         		"photo/10.jpg",
+         		"photo/11.jpg",
+         		"photo/12.jpg",
+         		"photo/13.jpg",
+         		"photo/14.jpg",
+         		"photo/15.jpg",
+         		"photo/16.jpg",
+         		"photo/17.jpg",
+         		"photo/18.jpg",
+         		"photo/19.jpg"
+		]
+
+		var imgs = '';
+		for( foto in fotos) {
+				imgs += "<img src=" + fotos[foto] + ">";
+		}
+
+		$(".galleria").html(imgs);
 });
